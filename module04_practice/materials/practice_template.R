27df43eb9251888f99dbb22fa677eb754e800e10
Hugo Tavares <hugo.m.tavares@gmail.com>
2018-04-05 16:40:25 +0100

    Fix typos

--- module04_practice/materials/practice_template.R
+++ module04_practice/materials/practice_template.R
@@ -1,24 +1,28 @@
-# Set up the environment ----
-# We will need to load these 4 libraries
+# 1. Set up the environment ----
 
+# We will need to load these 3 libraries
 library(visdat) # for initial inspection of data
 library(tidyverse) # for manipulation of data
 library(plotly) # for building interactive plots
 
-# Download the data ----
+# Set your working directory to the course folder
+## using setwd()
+
+
+# 2. Read the data ----
 
 # Please, note, here we are using `read_tsv()` because original file is
 # tab-separated, not comma-separated ('csv')
 
 mydata <- read_tsv("../data/TPM-light-WT-17c-27c-RNA-seq-average-rep1-rep2_misexpressed.tsv")
 
-# Check that dataset does not contain obvious problems ---
+# 3. Check that dataset does not contain obvious problems ----
 
 # Guide questions:
 # - how many dimensions does the data have?
 # - what are the column names? 
-# - what the variables could contain?
-# - what are variable types?
+# - what kind of data do these variables contain?
+# - what are the variable types?
 # - are there any missing values?
 
 # Hint: check the functions that we have discussed in the module02:
@@ -27,12 +31,13 @@ mydata <- read_tsv("../data/TPM-light-WT-17c-27c-RNA-seq-average-rep1-rep2_misex
 
 
 
-# Data transforms ----
-# Now, let's transform our data. We will do it in several steps and 
+# 4. Data manipulation ----
+
+# Now, let's reshape our data. We will do it in several steps and 
 # ultimately we will generate a tidy dataset, suitable for applying ggplot 
 # functions.
 
-# 1. Transform dataset from wide to long format: ----
+# 4.1 Reshape dataset from wide to long format: ----
 # Hint: one of two: gather() or spread() should be able to help you.
 
 
@@ -44,7 +49,8 @@ mydata <- read_tsv("../data/TPM-light-WT-17c-27c-RNA-seq-average-rep1-rep2_misex
 
 
 
-# 2. Tidy varibales ----
+# 4.2 Tidy variables ----
+
 # You might have noticed that now you have a column that contains several 
 # variables cramped together: time and temperature. Let's split it in 4 
 # columns: "units", "genotype", "time" and "temperature" using separate() function.
@@ -58,13 +64,14 @@ mydata <- read_tsv("../data/TPM-light-WT-17c-27c-RNA-seq-average-rep1-rep2_misex
 
 
 
-# 3. Drop "units" and "genotype" columns - the values there are always the same.
+# 4.3 Drop "units" and "genotype" columns 
+# the values in these columns are always the same.
 # Hint: use select() function ----
 
 
 
 
-# 4. Filter rows with low expression values ----
+# 4.4 Filter rows with low expression values ----
 # When dealing with expression data we often have lots of genes that are barely 
 # expressed. Let's get rid of them, this should slightly reduce the size of our
 # dataset.
@@ -79,19 +86,22 @@ mydata <- read_tsv("../data/TPM-light-WT-17c-27c-RNA-seq-average-rep1-rep2_misex
 
 
 
-# Build plots ----
-# 1. Initial visualization ----
-# After all the transformations our dataset is ready to be plotted. Use any type
-# of visualization you find suitable to the problem to get a basic understanding
-# of the structure  of your data. 
-# Hint: initialize plot with ggplot(), specify axes and plot-wise aesthetics inside aes(), and select a suitable geom_*. When in doubt, have a look at the examples provided in the module02 materials. 
+# 5. Build plots ----
+
+# 5.1. Initial visualization ----
+# After all the transformations our dataset is ready to be plotted. 
+# For example: what is the distribution of TPM values in the 3 time points and two temperatures?
+## Hint: initialize plot with ggplot(), specify axes and plot aesthetics inside aes()
+## select a suitable geom_*. 
+## When in doubt, have a look at the examples provided in the module02 materials. 
 
 
 
 
 
-# 2. Look at youur favourite genes ----
-# Say, now we are interested in 10 specific genes and we want to visualize their # expression in different temperatures over time.
+# 5.2 Look at your favourite genes ----
+# Say, now we are interested in 10 specific genes and we want to visualize their 
+# expression in different temperatures over time.
 # here are our genes of interest:
 genes_of_interest <- c("AT1G67090", "AT5G19240", "AT1G31580", "AT3G12580",
                        "AT1G80920", "AT3G54660", "AT2G25110", "AT1G19530",
@@ -104,18 +114,20 @@ genes_of_interest <- c("AT1G67090", "AT5G19240", "AT1G31580", "AT3G12580",
 
 
 # Build line plots for every gene of interest, facet by gene_id. Can you put
-# measurments for both temperatures in a songle plot?
+# measurements for both temperatures in a single plot?
+## Hint: you will need to use the "group" aesthetic to tell geom_line which points should be connected to each other
+
+
+
+# 5.3 Save transformed data to file ----
 
 
 
 
-# 3. Save transformed data to file with write_csv() ----
 
-
-
-
-
-# Bonus challenge for superheroes: now do all the same steps, from data transformation to plotting with pipes (excluding exploratory stages)! ----
+# Bonus challenge for superheroes ----
+## now do all the same steps, from data transformation to plotting with pipes 
+## (excluding exploratory stages)!
 
 
 
